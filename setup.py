4675bc17c00249a50bd64c87a876dd8476166e22
Z1R343L <jan2705g@gmail.com>
2021-07-28 21:55:53 +0200

    init

---
+++ setup.py
@@ -0,0 +1,9 @@
+import setuptools
+
+setuptools.setup(
+    name="string_frame",
+    version="1.0.0",
+    author="Z1R343L",
+    url="https://github.com/Z1R343L/string_frame",
+    packages=setuptools.find_packages(),
+)
---
+++ string_frame/__init__.py
@@ -0,0 +1,32 @@
+styles = [
+    ["╔", "═", "╗", "║", "╚", "╝"],
+    ["╭", "─", "╮", "│", "╰", "╯"],
+    ["+", "-", "+", "|", "+", "+"],
+    ["#", "#", "#", "#", "#", "#"],
+]
+
+
+def string_frame(str, style: int = 0, padding: int = 1):
+    str_s = str.split("\n")
+    max_len = 0
+    for i in str_s:
+        current_len = len(i)
+        if current_len > max_len:
+            max_len = current_len
+    res = []
+    res.append(
+        styles[style][0]
+        + (styles[style][1] * (max_len + (padding * 2)))
+        + styles[style][2]
+    )
+    for i in str_s:
+        space = max_len - len(i) + padding
+        res.append(
+            styles[style][3] + (" " * padding) + i + (" " * space) + styles[style][3]
+        )
+    res.append(
+        styles[style][4]
+        + (styles[style][1] * (max_len + (padding * 2)))
+        + styles[style][5]
+    )
+    return "\n".join(res)
